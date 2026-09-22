2cf6378b36a8684852d783ce6c72a8b8c6af634e
Mohammed Bazed Ali <mohdbazedali00@gmail.com>
2026-09-22 18:07:59 +0000

    fix: disable aria2 optimize-concurrent-downloads to prevent 100% stall

--- aria-nox-nzb.sh
+++ aria-nox-nzb.sh
@@ -4,7 +4,7 @@ aria2c --allow-overwrite=true --auto-file-renaming=true --bt-enable-lpd=false --
        --bt-remove-unselected-file=true --bt-tracker="[$tracker_list]" --bt-max-peers=0 --enable-rpc=true \
        --rpc-max-request-size=1024M --max-connection-per-server=10 --max-concurrent-downloads=1000 --split=10 \
        --seed-ratio=0 --check-integrity=true --continue=true --daemon=true --disk-cache=40M --force-save=true \
-       --min-split-size=10M --follow-torrent=false --follow-metalink=false --check-certificate=false --optimize-concurrent-downloads=true \
+       --min-split-size=10M --follow-torrent=false --follow-metalink=false --check-certificate=false --optimize-concurrent-downloads=false \
        --http-accept-gzip=false --max-file-not-found=0 --max-tries=20  --peer-id-prefix=-qB4520- --reuse-uri=true \
        --content-disposition-default-utf8=true --user-agent=Wget/1.12 --peer-agent=qBittorrent/4.5.2 --quiet=true \
        --summary-interval=0 --max-upload-limit=1K
